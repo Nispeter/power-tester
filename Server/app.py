3c9be8b83ce202622c18e3edf7c01c6b5b75e294
Caripanistico <dcaripan@udec.cl>
2021-10-05 14:08:54 -0300

    Mas archivos

--- Server/app.py
+++ Server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, render_template
 from flask_cors import CORS
 
 
@@ -8,8 +8,16 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    return( "Hello, world!")
+    return render_template("index.html")
 
 @app.route('/image', methods=['GET'])
 def image():
     return send_file(f"static/favicon.ico", mimetype="image/gif")
+
+@app.route('/sendcode', methods=['POST'])
+def cap_code():
+    code = request.form['code']
+    f = open("test/demofile.cpp", "w", newline="\n")
+    f.write(code)
+    f.close()
+    return 'OK', 200
---
+++ demofile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+    std::string tmr = "Hello World!";
+    std::cout << tmr;
+
+    return 0;
+
+}
